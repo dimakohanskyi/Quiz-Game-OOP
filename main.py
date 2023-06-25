41edf69245bcb6cdf3be5db85b7b0cbe0606786e
dimakohanskyi <dimakohanskyi@gmail.com>
2023-06-25 15:28:45 +0300

    Finish work on features

---
+++ main.py
@@ -0,0 +1,43 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+
+question_bank = []
+
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(q_text=question_text, q_answer=question_answer)
+    question_bank.append(new_question)
+
+
+quiz = QuizBrain(question_bank)
+
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+
+print("You have compleated quiz!")
+print(f"You final score is {quiz.score}/{len(question_bank)}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
